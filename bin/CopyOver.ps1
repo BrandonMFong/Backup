00f57d1ae95aab6032fba1cb0265c3436846880e
BrandonMFong <fong.m.brandon97@gmail.com>
2020-07-22 15:35:25 -0700

    Only newer files

--- bin/CopyOver.ps1
+++ bin/CopyOver.ps1
@@ -1,8 +1,6 @@
 Param([Parameter(Mandatory)][ValidateSet('Push','Pull')][String]$Method)
 
 [System.Xml.XmlDocument]$CopyConfig = Get-Content $PSScriptRoot\CopyOver.xml;
-New-Item $MachineXml.LogPath -Force | Out-Null;
-
 [System.Boolean]$Found = $false;
 foreach($Machine in $CopyConfig.CopyOver.Machines.Machine)
 {
@@ -10,45 +8,14 @@ foreach($Machine in $CopyConfig.CopyOver.Machines.Machine)
 }
 if(!$Found){throw "Error in config";}
 
+New-Item $MachineXml.LogPath -Force | Out-Null;
+
 switch($Method)
 {
-  # Maybe I can use the time file to flag that I need to pull just like git
-  # https://winscp.net/eng/docs/faq_script_modified_files#relative_time_constraint
-  "Push"
-  {
-    # Check if need pull
-    # I am assuming the NOTEBOOK folder is a sub dir from both the local and remote configured dir
-    # Get time file
-    & "$($MachineXml.WinSCPVariables.Path)" `
-      /log="$($MachineXml.LogPath)" /ini=nul `
-      /command `
-        "open sftp://$($MachineXml.WinSCPVariables.Username):$($MachineXml.WinSCPVariables.Password)@$($MachineXml.WinSCPVariables.IP)/ -hostkey=`"`"$($MachineXml.WinSCPVariables.SSHKey)`"`" -rawsettings Cipher=`"`"aes,chacha20,3des,WARN,des,blowfish,arcfour`"`"" `
-        "lcd $($MachineXml.Local)\$($MachineXml.Item)\bin" `
-        "cd $($MachineXml.Remote)/$($MachineXml.Item)/bin" `
-        "get Time" `
-        "exit"
-
-    [String]$TimeFilePath = "$($MachineXml.Remote)\$($MachineXml.Item)\bin\Time";
-    [DateTime]$timestring = Get-Content $TimeFilePath; # Get datetime from time file
-
-    # if the file is null then you don't need to warn for pull
-    # this catches the initial state of this workflow
-    if(![string]::IsNullOrEmpty($timestring))
-    {
-      [DateTime]$RemoteTimeStamp = Get-Content $TimeFilePath; # Get datetime from time file
-      [DateTime]$LocalTimeStamp = $MachineXml.LastFetch; # Get datetime from config
-
-      # If the remote time file is greater than the timestamp on config, you need to pull
-      if($RemoteTimeStamp -gt $LocalTimeStamp)
-      {
-        # But if I have updated files locally and I pull then I will overwrite those updated files to the old state
-        throw "Please pull";
-      }
-    }
-    [String]$Command = "put $($MachineXml.Item)";
-  }
-  "Pull"{[String]$Command = "get $($MachineXml.Item)";}
+  "Push"{[String]$Command = "put -neweronly $($MachineXml.Item)";}
+  "Pull"{[String]$Command = "get -neweronly $($MachineXml.Item)";}
 }
+
 & "$($MachineXml.WinSCPVariables.Path)" `
   /log="$($MachineXml.LogPath)" /ini=nul `
   /command `
