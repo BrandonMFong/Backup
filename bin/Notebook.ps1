60ff8024d0f10cdbfbd997beab40563472146c16
BrandonMFong <fong.m.brandon97@gmail.com>
2020-07-30 16:38:09 -0700

    [Code] Using parameters to delete rather than config

--- bin/Notebook.ps1
+++ bin/Notebook.ps1
@@ -1,4 +1,4 @@
-Param([ValidateSet('Push','Pull')][String]$Method)
+Param([ValidateSet('Push','Pull','Delete')][String]$Method,[String]$FilePath,[Switch]$DeleteLocally)
 
 [System.Xml.XmlDocument]$CopyConfig = Get-Content $PSScriptRoot\Notebook.xml;
 [System.Boolean]$Found = $false;
@@ -13,45 +13,35 @@ New-Item $MachineXml.LogPath -Force | Out-Null; # Clearing log file
 # how to set it so that if delete on remote, it will delete locally
 # Sync command
 [String]$Command = $null;
-foreach($Item in $MachineXml.Items.Item)
+switch($Method)
 {
-  switch($Item.cmd)
+  "Push"{[String]$Command = "put -neweronly $($MachineXml.Item) -nopreservetime";}
+  "Pull"{[String]$Command = "get -neweronly $($MachineXml.Item) -nopreservetime";}
+  "Delete"
   {
-    # "Push"{[String]$Command = "put -neweronly $($MachineXml.Item) -nopreservetime";}
-    # "Pull"{[String]$Command = "get -neweronly $($MachineXml.Item) -nopreservetime";}
-    "PushPull"
-    {
-      switch($Method)
-      {
-        "Push"{[String]$Command = "put -neweronly $($Item.InnerText) -nopreservetime";}
-        "Pull"{[String]$Command = "get -neweronly $($Item.InnerText) -nopreservetime";}
-      }
-    }
-    "Delete"
-    {
-      [String]$Command = "rm $($Item.InnerText)";
-    }
-  }
+    if([string]::IsNullOrEmpty($FilePath)){throw "Please provide remote file path"}
 
-  # Execute the command on WinSCP
-  if(![string]::IsNullOrEmpty($Command))
-  {
-    & "$($MachineXml.WinSCPVariables.Path)" `
-      /log="$($MachineXml.LogPath)" /ini=nul `
-      /command `
-        "open sftp://$($MachineXml.WinSCPVariables.Username):$($MachineXml.WinSCPVariables.Password)@$($MachineXml.WinSCPVariables.IP)/ -hostkey=`"`"$($MachineXml.WinSCPVariables.SSHKey)`"`" -rawsettings Cipher=`"`"aes,chacha20,3des,WARN,des,blowfish,arcfour`"`"" `
-        "lcd $($MachineXml.Local)" `
-        "cd $($MachineXml.Remote)" `
-        "$($Command)" `
-        "exit"
-
-      # Delete locally 
-      if($Item.cmd -eq "Delete")
-      {
-        [String]$i = $Item.InnerText;
-        Remove-Item $($MachineXml.Local + "\" + $i.Replace("/","\")) -Verbose -Force -Recurse;
-      }
+    [String]$RemotePath = $MachineXml.Remote + $FilePath.Replace(".\","/").Replace("\","/");
+    if($DeleteLocally)
+    {
+      [String]$LocalPath = $MachineXml.Local + $FilePath.Replace(".\","\");
+      Remove-Item $LocalPath -Verbose -Recurse -Force;
+    }
+    [String]$Command = "rm $($RemotePath)";
   }
+}
+
+# Execute the command on WinSCP
+if(![string]::IsNullOrEmpty($Command))
+{
+  & "$($MachineXml.WinSCPVariables.Path)" `
+    /log="$($MachineXml.LogPath)" /ini=nul `
+    /command `
+      "open sftp://$($MachineXml.WinSCPVariables.Username):$($MachineXml.WinSCPVariables.Password)@$($MachineXml.WinSCPVariables.IP)/ -hostkey=`"`"$($MachineXml.WinSCPVariables.SSHKey)`"`" -rawsettings Cipher=`"`"aes,chacha20,3des,WARN,des,blowfish,arcfour`"`"" `
+      "lcd $($MachineXml.Local)" `
+      "cd $($MachineXml.Remote)" `
+      "$($Command)" `
+      "exit"
 }
 
 [int16]$winscpResult = $LastExitCode;
