abe242590ee04ebf8f7264cf89dcea2a9a784783
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-20 18:18:51 -0700

    Init to just backup method

--- bin/Notebook.ps1
+++ bin/Notebook.ps1
@@ -14,6 +14,7 @@ foreach($Machine in $CopyConfig.Notebook.Machines.Machine)
 if(!$Found){throw "Error in config";}
 
 New-Item $MachineXml.LogPath -Force | Out-Null; # Clearing log file
+[String]$RemotePath = $MachineXml.Remote + $FilePath.Replace(".\","/").Replace("\","/");
 
 # how to set it so that if delete on remote, it will delete locally
 # Sync command
@@ -27,7 +28,6 @@ switch($Method)
   {
     if([string]::IsNullOrEmpty($FilePath)){throw "Please provide remote file path"}
 
-    [String]$RemotePath = $MachineXml.Remote + $FilePath.Replace(".\","/").Replace("\","/");
     if($DeleteLocally)
     {
       [String]$LocalPath = $MachineXml.Local + $FilePath.Replace(".\","\");
@@ -38,17 +38,14 @@ switch($Method)
 }
 
 # Execute the command on WinSCP
-if(![string]::IsNullOrEmpty($Command))
-{
-  & "$($MachineXml.WinSCPVariables.Path)" `
-    /log="$($MachineXml.LogPath)" /ini=nul `
-    /command `
-      "open sftp://$($MachineXml.WinSCPVariables.Username):$($MachineXml.WinSCPVariables.Password)@$($MachineXml.WinSCPVariables.IP)/ -hostkey=`"`"$($MachineXml.WinSCPVariables.SSHKey)`"`" -rawsettings Cipher=`"`"aes,chacha20,3des,WARN,des,blowfish,arcfour`"`"" `
-      "lcd $($MachineXml.Local)" `
-      "cd $($MachineXml.Remote)" `
-      "$($Command)" `
-      "exit"
-}
+& "$($MachineXml.WinSCPVariables.Path)" `
+  /log="$($MachineXml.LogPath)" /ini=nul `
+  /command `
+    "open sftp://$($MachineXml.WinSCPVariables.Username):$($MachineXml.WinSCPVariables.Password)@$($MachineXml.WinSCPVariables.IP)/ -hostkey=`"`"$($MachineXml.WinSCPVariables.SSHKey)`"`" -rawsettings Cipher=`"`"aes,chacha20,3des,WARN,des,blowfish,arcfour`"`"" `
+    "lcd $($MachineXml.Local)" `
+    "cd $($MachineXml.Remote)" `
+    "rm $($RemotePath)/*" `
+    "exit"
 
 [int16]$winscpResult = $LastExitCode;
 if ($winscpResult -eq 0){Write-Host "Success";}
