19126b4c9797409eb8d858d1095b14ef1111e291
BrandonMFong <fong.m.brandon97@gmail.com>
2020-08-20 21:29:57 -0700

    template and new method

--- bin/Backup.ps1
+++ bin/Backup.ps1
@@ -3,39 +3,21 @@
   A way to keep my notes in sync
 #>
 
-Param([ValidateSet('Push','Pull','Delete')][String]$Method,[String]$FilePath,[Switch]$DeleteLocally)
-
-[System.Xml.XmlDocument]$CopyConfig = Get-Content $PSScriptRoot\Notebook.xml;
+[System.Xml.XmlDocument]$CopyConfig = Get-Content $PSScriptRoot\Backup.xml;
 [System.Boolean]$Found = $false;
-foreach($Machine in $CopyConfig.Notebook.Machines.Machine)
+foreach($Machine in $CopyConfig.App.Machines.Machine)
 {
   if($Machine.ComputerName -eq $env:COMPUTERNAME){[System.Xml.XmlElement]$MachineXml = $Machine;$Found = $true;}
 }
 if(!$Found){throw "Error in config";}
 
 New-Item $MachineXml.LogPath -Force | Out-Null; # Clearing log file
-[String]$RemotePath = $MachineXml.Remote + $FilePath.Replace(".\","/").Replace("\","/");
+[String]$RemotePath = $MachineXml.Remote + "/" + $MachineXml.Item; # Getting file path with linux syntax 
 
 # how to set it so that if delete on remote, it will delete locally
 # Sync command
-[String]$Command = $null;
-switch($Method)
-{
-  "Push"{[String]$Command = "put -neweronly $($MachineXml.Item) -nopreservetime";}
-  "Pull"{[String]$Command = "get -neweronly $($MachineXml.Item) -nopreservetime";}
-  # You have to provide the file path from root of this repo
-  "Delete"
-  {
-    if([string]::IsNullOrEmpty($FilePath)){throw "Please provide remote file path"}
-
-    if($DeleteLocally)
-    {
-      [String]$LocalPath = $MachineXml.Local + $FilePath.Replace(".\","\");
-      Remove-Item $LocalPath -Verbose -Recurse -Force;
-    }
-    [String]$Command = "rm $($RemotePath)";
-  }
-}
+[String]$PutCommand = "put -neweronly $($MachineXml.Item) -nopreservetime";
+[String]$RemoveCommand = "rm $($RemotePath)/*";
 
 # Execute the command on WinSCP
 & "$($MachineXml.WinSCPVariables.Path)" `
@@ -44,7 +26,8 @@ switch($Method)
     "open sftp://$($MachineXml.WinSCPVariables.Username):$($MachineXml.WinSCPVariables.Password)@$($MachineXml.WinSCPVariables.IP)/ -hostkey=`"`"$($MachineXml.WinSCPVariables.SSHKey)`"`" -rawsettings Cipher=`"`"aes,chacha20,3des,WARN,des,blowfish,arcfour`"`"" `
     "lcd $($MachineXml.Local)" `
     "cd $($MachineXml.Remote)" `
-    "rm $($RemotePath)/*" `
+    "$($RemoveCommand)" `
+    "$($PutCommand)" `
     "exit"
 
 [int16]$winscpResult = $LastExitCode;
